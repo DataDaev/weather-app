b3a3b1f3bc2eb37722009f3b3a55a1a826c5c75d
DataDaev <Datadaev@gmail.com>
2023-12-04 21:03:36 -0700

    Created WeatherAnimation component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import Lottie from "lottie-react";
 import Header from "./Header";
-import animationData from "./assets/part-cloudy-animation.json";
 import { useEffect, useState } from "react";
+import WeatherAnimation from "./WeatherAnimation";
 
 const api = {
   key: "624d764ffa0f7674fc5785fe8404a689",
@@ -12,7 +11,7 @@ export default function App() {
   const [search, setSearch] = useState("");
   const [weather, setWeather] = useState({});
   const convertedTemp = (weather.main?.temp - 273.15) * (9 / 5) + 32; //Converted Kelvin to Fahrenheit
-
+  const currentWeather = weather.main && weather.weather[0].main;
   async function weatherData() {
     try {
       const res = await fetch(
@@ -20,14 +19,15 @@ export default function App() {
       );
       const data = await res.json();
       setWeather(data);
+      console.log(data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
   }
 
-  useEffect(function () {
-    weatherData();
-  }, []);
+  // useEffect(function () {
+  //   weatherData();
+  // }, []);
 
   function handleKeyPress(e) {
     if (e.key === "Enter") {
@@ -42,14 +42,13 @@ export default function App() {
       <div className="weather-info">
         {weather.main && (
           <div className="weather-animation">
-            {weather.weather[0].main.includes("Cloud") && (
-              <Lottie animationData={animationData} />
-            )}
+            <WeatherAnimation weatherType={currentWeather} />
           </div>
         )}
         <div className="search">
           <input
             className="search-bar"
+            placeholder="City (Ex: Phoenix)"
             type="text"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
